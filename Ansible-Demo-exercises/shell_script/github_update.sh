e84ce8e52abf26582f3d9c27ac722594610bb0ba
Pulltikurthi Franklin Ashok  (NCS) <franklinpashok@gmail.com>
2021-07-15 16:56:11 +0800

    shell script to run git commands

---
+++ Ansible-Demo-exercises/shell_script/github_update.sh
@@ -0,0 +1,8 @@
+
+git switch officelaptop
+git status
+git add Ansible-Demo-exercises/
+git commit -m 'merged with homelaptop'
+git push -u origin officelaptop
+git switch master
+git push -u origin master
